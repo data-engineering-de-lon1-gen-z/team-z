86044cdb6fc6d1b7b4902307b80b571e09280fb2
Matthew Murray <matt@compti.me>
2020-10-08 15:19:41 +0100

    Session context manager

--- src/load.py
+++ src/load.py
@@ -1,3 +1,5 @@
+from contextlib import contextmanager
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.engine import Engine
@@ -7,27 +9,43 @@ from src.models import Base, Product, Location, Basket, Transaction
 
 # TODO from src.config import MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD
 
+engine: Engine = create_engine(
+    "mysql+pymysql://root:password@localhost:33066/dev?charset=latin1"
+)
+Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_context_manager():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+
+def insert_many(session, data: list):
+    session.add_all(data)
+
+
 if __name__ == "__main__":
-    engine: Engine = create_engine(
-        "mysql+pymysql://root:password@localhost:33066/dev?charset=latin1"
-    )
     # Create database if it does not already exist
     if not database_exists(engine.url):
         create_database(engine.url)
 
-    engine.connect()
-
+    # Create all the tables
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
 
     from uuid import uuid4
 
     product = Product(id=str(uuid4()), name="Lemonade", iced=False, price=1.25)
-    session.add(product)
+    with session_context_manager() as session:
+        insert_many(session, [product])
 
-    # TODO Don't forget to commit
-
-    queried_product = session.query(Product).first()
-    print(queried_product.name)
+        # Select query on product table, get the first result
+        queried_product = session.query(Product).first()
+        print(queried_product.name)
